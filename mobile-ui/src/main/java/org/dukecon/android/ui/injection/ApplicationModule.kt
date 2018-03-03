7155c94c6b640dd10500cc1c2dff6ac620a9de36
Michal Harakal <michal.harakal@googlemail.com>
2018-03-03 11:40:07 +0100

    fixe error in api endpint url

--- mobile-ui/src/main/java/org/dukecon/android/ui/injection/ApplicationModule.kt
+++ mobile-ui/src/main/java/org/dukecon/android/ui/injection/ApplicationModule.kt
@@ -127,7 +127,7 @@ open class ApplicationModule {
     @Provides
     internal fun provideConfernceService(client: OkHttpClient, gson: Gson): ConferencesApi {
         var restAdapter = Retrofit.Builder() // https://latest.dukecon.org/javaland/2018/rest/conferences/javaland2018/events
-                .baseUrl("https://programm.javaland.eu/2018/rest") //https://latest.dukecon.org/javaland/2018/rest/") //endpoitUrlProvider.getUrl())
+                .baseUrl("https://programm.javaland.eu/2018/rest/") //https://latest.dukecon.org/javaland/2018/rest/") //endpoitUrlProvider.getUrl())
                 .client(client)
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create(gson))
