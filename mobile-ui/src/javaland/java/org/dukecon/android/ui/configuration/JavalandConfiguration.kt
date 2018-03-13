041bb76eaaff011cb707b71bf3a5df37fd9b3d88
Michal Harakal <michal.harakal@googlemail.com>
2018-03-13 01:20:39 +0100

    moves ConferenceConfiguration to org.dukecon.android.ui.configuration packagename

---
+++ mobile-ui/src/javaland/java/org/dukecon/android/ui/configuration/JavalandConfiguration.kt
@@ -0,0 +1,11 @@
+package org.dukecon.android.ui.configuration
+
+import android.app.Application
+import org.dukecon.android.ui.app.ConferenceConfiguration
+
+class JavalandConfiguration(val application: Application) : ConferenceConfiguration {
+    override val baseUrl: String
+        get() = "https://programm.javaland.eu/2018/rest/" //https://latest.dukecon.org/javaland/2018/rest/") //endpoitUrlProvider.getUrl())
+    override val conferenceId: String
+        get() = "javaland2018"
+}
--- mobile-ui/src/javaland/java/org/dukecon/android/ui/injection/ConferenceModule.kt
+++ mobile-ui/src/javaland/java/org/dukecon/android/ui/injection/ConferenceModule.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import dagger.Module
 import dagger.Provides
 import org.dukecon.android.ui.app.ConferenceConfiguration
+import org.dukecon.android.ui.configuration.JavalandConfiguration
 
 @Module
 open class ConferenceModule {
